7359c48bac9d4f8012646b231163d48d0a4360ea
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-10-30 10:27:09 -0500

    feat: initial migrations

--- WebApiAutores/Startup.cs
+++ WebApiAutores/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApiAutores;
 
@@ -16,6 +17,9 @@ public class Startup
     // Add services to the container.
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     services.AddControllers();
+    services.AddDbContext<ApplicationDBContext>(options =>
+      options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+    );
     services.AddEndpointsApiExplorer();
     services.AddSwaggerGen();
 
