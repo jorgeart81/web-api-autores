f1fcb41687adc1a62643136d138984e494758f7a
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-11-03 00:11:32 -0500

    update author

--- WebApiAutores/Controllers/AuthorsController.cs
+++ WebApiAutores/Controllers/AuthorsController.cs
@@ -15,11 +15,34 @@ public class AuthorsController(ApplicationDBContext context) : ControllerBase
     return await context.Authors.ToListAsync();
   }
 
+  [HttpGet("{id:int}")]
+  public async Task<ActionResult<List<Author>>> GetById(int id)
+  {
+    var author = await context.Authors.FindAsync(id);
+
+    if (author == null) return NotFound();
+
+    return Ok(author);
+  }
+
   [HttpPost]
   public async Task<ActionResult<Author>> Post(Author author)
   {
     context.Add(author);
     await context.SaveChangesAsync();
+    return Ok();
+  }
+
+  [HttpPut("{id:int}")]
+  public async Task<ActionResult<Author>> Put(Author author, int id)
+  {
+    var existingAuthor = await context.Authors.FindAsync(id);
+    if (existingAuthor == null) return NotFound();
+
+    existingAuthor.Name = author.Name;
+    context.Entry(existingAuthor).State = EntityState.Modified;
+    await context.SaveChangesAsync();
+
     return Ok();
   }
 }
