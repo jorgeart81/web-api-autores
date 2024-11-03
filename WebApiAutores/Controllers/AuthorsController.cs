41ccd1e2d4f45e9195665480bf0ddd4a6d9e5d48
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-11-02 23:27:45 -0500

    create author

--- WebApiAutores/Controllers/AuthorsController.cs
+++ WebApiAutores/Controllers/AuthorsController.cs
@@ -1,19 +1,25 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApiAutores.Entities;
 
 namespace WebApiAutores.Controllers;
 
 [ApiController]
 [Route("api/authors")]
-public class AuthorsController : ControllerBase
+public class AuthorsController(ApplicationDBContext context) : ControllerBase
 {
   [HttpGet]
-  public ActionResult<List<Author>> Get()
+  public async Task<ActionResult<List<Author>>> Get()
   {
-    return Ok(new List<Author> {
-      new() { Id = 1, Name= "Jorge" },
-      new() { Id = 2, Name= "Andrés" },
-    });
+    return await context.Authors.ToListAsync();
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<Author>> Post(Author author)
+  {
+    context.Add(author);
+    await context.SaveChangesAsync();
+    return Ok();
   }
 }
