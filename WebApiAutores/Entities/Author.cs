75c3121c62f4261e1921c29eb4be927054a85502
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-11-05 16:32:12 -0500

    validation by model

--- WebApiAutores/Entities/Author.cs
+++ WebApiAutores/Entities/Author.cs
@@ -1,15 +1,27 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using WebApiAutores.Validations;
 
 namespace WebApiAutores.Entities;
 
-public class Author
+public class Author : IValidatableObject
 {
   public int Id { get; set; }
 
   [Required]
-  [FirstCapitalLetter]
+  [StringLength(maximumLength: 120, MinimumLength = 3)]
   public required string Name { get; set; }
   public List<Book>? Books { get; set; }
+
+  public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+  {
+    if (!string.IsNullOrEmpty(Name))
+    {
+      var firstLetter = Name.ToString()[0].ToString();
+      if (firstLetter != firstLetter.ToUpper())
+      {
+        yield return new ValidationResult("The first letter must be capitalized", new string[] { nameof(Name) });
+      }
+    }
+
+  }
 }
--- WebApiAutores/Entities/Book.cs
+++ WebApiAutores/Entities/Book.cs
@@ -1,10 +1,16 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using WebApiAutores.Validations;
 
 namespace WebApiAutores.Entities;
 
 public class Book
 {
   public int Id { get; set; }
+
+  [Required]
+  [FirstCapitalLetter]
+  [StringLength(maximumLength: 120)]
   public required string Title { get; set; }
   public int AuthorId { get; set; }
   public Author? Author { get; set; }
