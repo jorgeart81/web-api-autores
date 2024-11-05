d7b7fef4a19dd0825eb72dad8d0230f8319226c4
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-11-05 15:53:17 -0500

    add validation attribute

--- WebApiAutores/Entities/Author.cs
+++ WebApiAutores/Entities/Author.cs
@@ -1,10 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using WebApiAutores.Validations;
 
 namespace WebApiAutores.Entities;
 
 public class Author
 {
   public int Id { get; set; }
+
+  [Required]
+  [FirstCapitalLetter]
   public required string Name { get; set; }
   public List<Book>? Books { get; set; }
 }
---
+++ WebApiAutores/Validations/FirstCapitalLetter.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiAutores.Validations;
+
+public class FirstCapitalLetter : ValidationAttribute
+{
+  protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+  {
+    if (value == null || string.IsNullOrEmpty(value.ToString()))
+    {
+      return ValidationResult.Success;
+    }
+
+    var firstLetter = value.ToString()[0].ToString();
+    if (firstLetter != firstLetter.ToUpper())
+    {
+      return new ValidationResult("The first letter must be capitalized");
+    }
+
+    return ValidationResult.Success;
+  }
+}
