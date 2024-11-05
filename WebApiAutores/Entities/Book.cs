98b44bddca939fdf261342080f92133a054f2009
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-11-05 11:40:09 -0500

    add Book entity

---
+++ WebApiAutores/Entities/Book.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WebApiAutores.Entities;
+
+public class Book
+{
+  public int Id { get; set; }
+  public required string Title { get; set; }
+  public int AuthorId { get; set; }
+  public Author? Author { get; set; }
+
+}
