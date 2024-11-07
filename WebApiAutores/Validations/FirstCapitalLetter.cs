d6f844d14a3db8029f76a8af68119dd92cb0a243
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-11-07 18:01:45 -0500

    fix: posible null value

--- WebApiAutores/Validations/FirstCapitalLetter.cs
+++ WebApiAutores/Validations/FirstCapitalLetter.cs
@@ -7,13 +7,13 @@ public class FirstCapitalLetter : ValidationAttribute
 {
   protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
   {
-    if (value == null || string.IsNullOrEmpty(value.ToString()))
+    if (value == null || string.IsNullOrEmpty(value?.ToString()))
     {
       return ValidationResult.Success;
     }
 
-    var firstLetter = value.ToString()[0].ToString();
-    if (firstLetter != firstLetter.ToUpper())
+    var firstLetter = value?.ToString()?[0].ToString();
+    if (firstLetter != firstLetter?.ToUpper())
     {
       return new ValidationResult("The first letter must be capitalized");
     }
