7ef77844d20aaa4d6689a2900e3680ce8e8e97fd
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2024-11-08 21:02:38 -0500

    add CreateBookDTO

---
+++ WebApiAutores/DTOs/CreateBookDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using WebApiAutores.Validations;
+
+namespace WebApiAutores.DTOs;
+
+public class CreateBookDTO
+{
+  [Required]
+  [FirstCapitalLetter]
+  [StringLength(maximumLength: 255)]
+  public required string Title { get; set; }
+}
