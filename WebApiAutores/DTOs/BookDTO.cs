4cffcd3b84c5b59db8cc9bd553458f43d31cc739
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-11-16 11:48:26 -0500

    set Authors prop nullable

--- WebApiAutores/DTOs/BookDTO.cs
+++ WebApiAutores/DTOs/BookDTO.cs
@@ -6,6 +6,6 @@ public class BookDTO
 {
   public int Id { get; set; }
   public required string Title { get; set; }
-  public required List<AuthorDTO> Authors { get; set; }
+  public  List<AuthorDTO>? Authors { get; set; }
   // public List<CommentDTO>? Comments { get; set; }
 }
